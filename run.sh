a2fa579e825a9713a7963b5177bd49abc15f33b4
Mike Ellis <mtcanuck1@gmail.com>
2020-12-22 13:13:29 -0500

    Added environment variable pass through

--- run.sh
+++ run.sh
@@ -3,7 +3,9 @@
 # Start the run once job.
 echo "Docker container has been started"
 # Setup a cron schedule
-echo "* * * * * /usr/local/bin/cron.sh >> /var/log/cron.log 2>&1
+# * * * * * = Every Minute
+# */2 * * * * = Every Two Minutes
+echo "*/2 * * * * export USERNAME=$USERNAME; export PASSWORD=$PASSWORD; export DOMAIN=$DOMAIN;  /usr/local/bin/cron.sh >> /var/log/cron.log 2>&1
 # This extra line makes it a valid cron" > scheduler.txt
 crontab scheduler.txt
 cron -f
